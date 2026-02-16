3489a15289ec55fe3f97f9d5c20c0df9b589d6c1
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-02-16 23:38:35 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char a;
+    scanf("%c", &a);
+    if(a == 'N' || a == 'n') {
+        printf("Naver D2");
+    }
+    else printf("Naver Whale");
+    return 0;
+}
