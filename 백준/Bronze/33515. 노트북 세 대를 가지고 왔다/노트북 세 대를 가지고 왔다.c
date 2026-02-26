3a0bd5ed19339b91ffb7a6cff601e15a54e425fb
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-02-26 22:55:31 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.c
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int t1, t2;
-    scanf("%d %d", &t1, &t2);
-    printf("%d", t1<t2?t1:t2);
-    return 0;
-}
+main(a,b) {scanf("%d%d",&a,&b);printf("%d",a<b?a:b);}
