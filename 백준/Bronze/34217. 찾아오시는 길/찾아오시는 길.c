1e3ece4e80a4a7539ea635b0e17b811e40331308
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-03-09 23:23:32 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.c
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int a,b,c,d;
-    scanf("%d%d%d%d",&a,&b,&c,&d);
-    puts(a+c<b+d?"Hanyang Univ.":a+c>b+d?"Yongdap":"Either");
-    return 0;
-}
+main(a,b,c,d){scanf("%d%d%d%d",&a,&b,&c,&d);puts(a+c<b+d?"Hanyang Univ.":a+c>b+d?"Yongdap":"Either");}
