8e71ec88bcbd74f832a2d0d9fc62903125081604
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-03-11 23:41:57 +0900

    [Bronze V] Title: Arno's Sleep Schedule, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/29863. Arno's Sleep Schedule/Arno's Sleep Schedule.c
+++ 백준/Bronze/29863. Arno's Sleep Schedule/Arno's Sleep Schedule.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",19<a?24-a+b:b-a);
-    return 0;
-}
+main(a,b){scanf("%d%d",&a,&b);printf("%d",19<a?24-a+b:b-a);}
