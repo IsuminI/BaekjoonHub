72b957b60ec5bf3adc9d4931ce5daba9a1418adb
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-03-12 23:01:35 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.c
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
-    printf("%d",3*a+4*b+5*c);
-    return 0;
-}
+main(a,b,c){scanf("%d%d%d",&a,&b,&c);printf("%d",3*a+4*b+5*c);}
