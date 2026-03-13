10b514e98359cbcf3cf4747fe8a89dd97e774548
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-03-13 22:52:38 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.c
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",a*1000+b*10000);
-    return 0;
-}
+main(a,b){scanf("%d%d",&a,&b);printf("%d000",a+b*10);}
