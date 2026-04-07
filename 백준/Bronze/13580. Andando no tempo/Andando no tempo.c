0d980420b6658604d9fb2860c6c4087edc601b73
lsuminl <149383661+IsuminI@users.noreply.github.com>
2026-04-07 22:06:32 +0900

    [Bronze IV] Title: Andando no tempo, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/13580. Andando no tempo/Andando no tempo.c
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    if(a==b+c||b==a+c||c==a+b) puts("S");
-    else if(a==b||a==c||b==c) puts("S");
-    else puts("N");
-    return 0;
-}
+main(a,b,c){scanf("%d%d%d",&a,&b,&c);puts(a==b||a==c||b==c||a==b+c||b==a+c||c==a+b?"S":"N");}
